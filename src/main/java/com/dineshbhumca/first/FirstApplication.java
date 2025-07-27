0e6d9025cb25f198674ebfb206a7195456a18bb8
dineshkumarsah <dineshdqg@gmail.com>
2025-07-27 13:00:17 +0530

    added print

--- src/main/java/com/dineshbhumca/first/FirstApplication.java
+++ src/main/java/com/dineshbhumca/first/FirstApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstApplication {
 
 	public static void main(String[] args) {
+		System.out.println("dinesh");
 		SpringApplication.run(FirstApplication.class, args);
 	}
-
 }
