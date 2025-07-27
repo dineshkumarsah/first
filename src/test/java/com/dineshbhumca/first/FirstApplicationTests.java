0a350c7bb910e1cdd78b4d81f56c3f1e9a54d7d0
dineshkumarsah <dineshdqg@gmail.com>
2025-07-27 11:18:40 +0530

    intial commit

---
+++ src/test/java/com/dineshbhumca/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineshbhumca.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
